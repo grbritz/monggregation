d35bc41aa51ebb4c9a046228ac8df448f26d7853
Graeme Britz <grbritz@gmail.com>
2015-05-08 15:04:05 -0700

    show/hide collections

--- public/js/main.js
+++ public/js/main.js
@@ -16,17 +16,58 @@ function DatabaseMenu () {
         return;
       }
 
-      // TODO: Something not as abrasive
-      self.databases.removeAll();
+      _.each(data.databases, function(databaseData, index) {
+        // TODO: make not n^2
+        var existingMenuItem = _.filter(self.databases, function(databaseMenuItem) {
+          return databaseMenuItem.name === databaseData.name;
+        })[0];
 
-      _.each(data.databases, function(database, index) {
-        self.databases.push(database);
+        if (existingMenuItem) {
+          existingMenuItem.updateConfig(databaseData);
+        } else {
+          self.databases.push(new DatabaseMenuItem(databaseData));
+        }
       });
     }, "json");
   };
 
-
   self.fetchDatabases();
+}
+
+function DatabaseMenuItem(config) {
+  var self = this;
+  self.showCollections = ko.observable(false);
+  self.collections = ko.observableArray();
+
+  self.toggleShowCollections = function () {
+    self.showCollections(!self.showCollections());
+
+    if (self.showCollections) {
+      self.fetchCollections();
+    }
+  };
+
+  self.fetchCollections = function () {
+    console.log("fecthingColleftions")
+    $.post("/getCollections", {database: self.name}, function(data) {
+      if (!data) {
+        console.error("Fetch collections for " + self.name + " failed");
+        return;
+      }
+
+      self.collections.removeAll();
+      _.each(data, function(collectionName, index) {
+        self.collections.push({name: collectionName});
+      });
+    });
+  };
+
+  self.updateConfig = function (config) {
+    self.name = config.name;
+    self.sizeOnDisk = config.sizeOnDisk;
+  };
+
+  self.updateConfig(config);
 }
 
 
